2a5988fb55798f6adf6ca4385d049ac06a1bbcbb
jgenero <jeandamien.genero@gmail.com>
2021-02-04 20:55:53 +0100

    working on search route and result rout

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,65 @@ author = Jean-Damien Généro
 date = 2020-12-15
 """
 
-
 # libraries
 import csv
+import os
 from bs4 import BeautifulSoup
-from flask import render_template
+from flask import render_template, request
 from lxml import etree
 
 
 # imports
 from .app import app
 from .clear_xml import clear_file
+
+
+# functions
+def get_filenames():
+    """
+    Making a list with files paths.
+    :return: a dict
+    :rtype: list
+    """
+    file_paths_ls = []
+    for file in os.listdir("./app/static/xml"):
+        if file.endswith(".xml"):
+            file_paths_ls.append(os.path.join("./app/static/xml", file))
+    return file_paths_ls
+
+
+def get_subtypes(files_list):
+    proprietes_st = []
+    travaux_st = []
+    industries_st = []
+    par_10_st = []
+    subtypes_dict = {}
+    subtypes = []
+    for item in files_list:
+        with open(item) as xml:
+            xmlfile = xml.read()
+            soup = BeautifulSoup(xmlfile, 'xml')
+            subtypes = soup.find_all('div', subtype=True)
+            for tag in subtypes:
+                if tag["type"] == "proprietes":
+                    proprietes_st.append(tag["subtype"])
+                elif tag["type"] == "travaux":
+                    travaux_st.append(tag["subtype"])
+                elif tag["type"] == "industries":
+                    industries_st.append(tag["subtype"])
+                elif tag["type"] == "par_10":
+                    par_10_st.append(tag["subtype"])
+    # subtypes_dict["proprietes"] = set(proprietes_st)
+    # subtypes_dict["travaux"] = set(travaux_st)
+    # subtypes_dict["industries"] = set(industries_st)
+    # subtypes_dict["par_10"] = set(par_10_st)
+    properties  = list(set(proprietes_st))
+    works = list(set(travaux_st))
+    industries = list(set(industries_st))
+    assets = list(set(par_10_st))
+    # return subtypes_dict
+    subtypes = [properties, works, industries, assets]
+    return subtypes
 
 
 # routes
@@ -65,6 +113,20 @@ def txt_mono(mono_id):
 
 @app.route("/search")
 def search():
-    all_sections = {"Propriétés": "proprietes", "Travaux": "travaux", "Industries": "industries",
-    "Habitation, mobilier et vêtement": "par_10"}
-    return render_template("search.html", ls_s=all_sections)
+    # all_sections = {"Propriétés": "1", "Travaux": "2", "Industries": "3", "Habitation, mobilier et vêtement": "4"}
+    all_sections = ["Propriétés", "Travaux", "Industries", "Habitation, mobilier et vêtement"]
+    all_subtypes = get_subtypes(get_filenames())
+    number = -1
+    subdict = {}
+    for sublist in all_subtypes:
+        number += 1
+        for subtype in sublist:
+            subdict[subtype] = str(number)
+    return render_template("search.html", ls_s=all_sections, subtypes=subdict)
+
+@app.route("/search/results")
+def results():
+    motclef = request.args.get("keyword", None)
+    # il faut définir une variable résult où sera stockée la liste des sections à afficher
+    # le keyword est la valeur du xpath (mais il faut deux valeurs ?)
+    return render_template("results.html")
