412da63b5b57e95467c086a32033655399927860
Jean-Damien Généro <jeandamien.genero@gmail.com>
2021-07-28 18:17:33 +0200

    initial commit

---
+++ app/static/python/persname_id.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Author : Jean-Damien Généro
+Affiliation : French National Center for Scientific Research (CNRS)
+Assigned at the Centre de recherches historiques (CRH, UMR 8558)
+Date : 2021-07-28
+Update :
+"""
