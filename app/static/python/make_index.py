21394a35039c7f84263d3e070840a14f880ce2f5
Jean-Damien Généro <jeandamien.genero@gmail.com>
2021-07-28 17:59:04 +0200

    update date + affiliation

--- app/static/python/make_index.py
+++ app/static/python/make_index.py
@@ -3,8 +3,10 @@
 
 """
 Author : Jean-Damien Généro
+Affiliation : French National Center for Scientific Research (CNRS)
+Assigned at the Centre de recherches historiques (CRH, UMR 8558)
 Date : 2020-06-15
-Update :
+Update : 2021-07-28
 """
 
 import csv
